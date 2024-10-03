30ff5c5d19828001d669cee0b1cbefc23a88652a
rafaelmorgado6 <rafa.morgado@ua.pt>
2024-10-03 09:48:24 +0100

    Aula3

---
+++ Parte_03/ex1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from time import time
+print("Time since 1/Jan/1970: " + str(time()) + " seconds")
+
+from time import time, ctime
+print("Today's date: " + str(ctime()))
+
+def tic():
+    global start_time
+    
+
+def toc()
